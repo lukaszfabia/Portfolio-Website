e6708d34949402156c8adbf149bb9da0c8d45cad
LukaszAcer <ufabia03@gmail.com>
2023-07-16 21:06:16 +0200

    naprawiony bar

--- hello.js
+++ hello.js
@@ -1,5 +1,18 @@
-burger = document.querySelector('.burger-menu')
-burger.onclick=function (){
-    let bar = document.querySelector('.nav-links');
-    bar.classList.toggle('active')
-}
+const burger = document.querySelector('.burger-menu');
+const socialLinks = document.querySelectorAll('.social-link');
+
+let isExpanded = false;
+
+burger.addEventListener('click', () => {
+    const navLinks = document.querySelector('.nav-links');
+    const card = document.querySelector('.card');
+
+    navLinks.classList.toggle('active');
+    card.style.display = isExpanded ? 'flex' : 'none';
+
+    socialLinks.forEach((link) => {
+        link.style.display = isExpanded ? 'inline-block' : 'none';
+    });
+
+    isExpanded = !isExpanded;
+});
